b2bbfeef54d639c604ba459c91817e56439e5948
hai tong yan <1770115879@qq.com>
2024-10-16 12:08:50 -0700

    fix login bugs

--- 03-frontend/react-library/src/lib/oktaConfig.ts
+++ 03-frontend/react-library/src/lib/oktaConfig.ts
@@ -1,5 +1,5 @@
 export const oktaConfig = {
-    clientId: '0oakc64vk6p6O9ywG5d7',
+    clientId: '0oakej6ys3glIWCRJ5d7',
     issuer: 'https://dev-11954395.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],

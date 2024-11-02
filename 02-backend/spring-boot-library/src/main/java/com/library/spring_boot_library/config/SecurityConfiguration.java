f6318337a79246377efa4f3db401a602dd7c4424
hai tong yan <1770115879@qq.com>
2024-11-02 16:56:40 -0700

    add security stuff

---
+++ 02-backend/spring-boot-library/src/main/java/com/library/spring_boot_library/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.library.spring_boot_library.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // disable cross site request forgery
+        httpSecurity.csrf().disable();
+        // protect endpoints at /api/<type>/secure
+        httpSecurity.authorizeRequests(configurer ->
+                configurer
+                        .antMatchers("/api/books/secure/**")
+                        .authenticated())
+                        .oauth2ResourceServer()
+                .jwt();
+        // add CORS filters
+        httpSecurity.cors();
+        // add content negotiation strategy
+        httpSecurity.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+        // force a non-empty response body for 401's to make the response friendly
+        Okta.configureResourceServer401ResponseBody(httpSecurity);
+        return httpSecurity.build();
+    }
+}
--- 02-backend/spring-boot-library/src/main/java/com/library/spring_boot_library/controller/BookController.java
+++ 02-backend/spring-boot-library/src/main/java/com/library/spring_boot_library/controller/BookController.java
@@ -2,6 +2,7 @@ package com.library.spring_boot_library.controller;
 
 import com.library.spring_boot_library.entity.Book;
 import com.library.spring_boot_library.service.BookService;
+import com.library.spring_boot_library.utils.ExtractJwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,20 +18,22 @@ public class BookController {
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token,
+                             @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJwt.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkedoutByUser(@RequestParam Long bookId) {
-        String userEmail = "testuser@email.com";
+    public Boolean checkedoutByUser(@RequestHeader(value = "Authorization") String token,
+                                    @RequestParam Long bookId) {
+        String userEmail = ExtractJwt.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "testuser@email.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJwt.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 }
